730c06d2725fac21eab88442721754ee0e41c2ae
maruri6797 <138319041+maruri6797@users.noreply.github.com>
2023-08-12 15:12:30 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
