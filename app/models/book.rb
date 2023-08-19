5e2248f11a63265d4b8d1af78f22c9a571ec8aa6
maruri6797 <138319041+maruri6797@users.noreply.github.com>
2023-08-19 08:43:00 +0000

    [Finish]7-b

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,9 +6,14 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true,length:{maximum:200}
-  
+
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: Time.zone.yesterday.all_day) }
+  scope :created_two_days_ago, -> { where(created_at: 2.days.ago.all_day) }
+  scope :created_three_days_ago, -> { where(created_at: 3.days.ago.all_day) }
+  scope :created_four_days_ago, -> { where(created_at: 4.days.ago.all_day) }
+  scope :created_five_days_ago, -> { where(created_at: 5.days.ago.all_day) }
+  scope :created_six_days_ago, -> { where(created_at: 6.days.ago.all_day) }
   scope :created_last_week, -> { where(created_at: Time.current.last_week.all_week) }
   scope :created_this_week, -> { where(created_at: Time.current.all_week) }
 
